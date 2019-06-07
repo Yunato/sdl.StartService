720c68575f8306f1182138c157f9c6d54cc4415e
Yunato <yunato525@gmail.com>
2019-06-07 17:09:12 +0900

    Add Service3 & Add BroadcastReceiver at MainActivity

--- app/src/main/java/jp/ac/titech/itpro/sdl/startservice/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/startservice/MainActivity.java
@@ -1,19 +1,51 @@
 package jp.ac.titech.itpro.sdl.startservice;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private final static String TAG = MainActivity.class.getSimpleName();
+    private BroadcastReceiver receiver;
+    private IntentFilter filter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate in " + Thread.currentThread());
         setContentView(R.layout.activity_main);
+
+        receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Log.d(TAG, "BroadcastReceiver#onReceive in " + Thread.currentThread());
+                String action = intent.getAction();
+                if(action == Intent.ACTION_ANSWER && intent.getStringExtra(Service3.EXTRA_RTNARG) != null){
+                    Toast.makeText(MainActivity.this, "Goodbye, Service3", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+        filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_ANSWER);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(receiver, filter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
     }
 
     public void onClickTest1(View v) {
@@ -27,6 +59,13 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onClickTest2 in " + Thread.currentThread());
         Intent intent = new Intent(this, Service2.class);
         intent.putExtra(Service2.EXTRA_MYARG, "Hello, Service2");
+        startService(intent);
+    }
+
+    public void onClickTest3(View v){
+        Log.d(TAG, "onClickTest3 in " + Thread.currentThread());
+        Intent intent = new Intent(this, Service3.class);
+        intent.putExtra(Service3.EXTRA_MYARG, "Hello, Service3");
         startService(intent);
     }
 }
